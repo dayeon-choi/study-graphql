45a08aa5df67e1d92bee6dcf98876d1985cd58c5
dayeon <dayeon2399@naver.com>
2021-06-07 21:15:02 +0900

    1-3 실습완료

--- 1-3-graphql-exp/index.js
+++ 1-3-graphql-exp/index.js
@@ -1,44 +1,49 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require("apollo-server");
 
-const queries = require('./typedefs-resolvers/_queries')
-const mutations = require('./typedefs-resolvers/_mutations')
-const enums = require('./typedefs-resolvers/_enums')
-const teams = require('./typedefs-resolvers/teams')
-const people = require('./typedefs-resolvers/people')
-const roles = require('./typedefs-resolvers/roles')
-const equipments = require('./typedefs-resolvers/equipments')
-const softwares = require('./typedefs-resolvers/softwares')
-const supplies = require('./typedefs-resolvers/supplies')
-const tools = require('./typedefs-resolvers/tools')
-const givens = require('./typedefs-resolvers/givens')
+const queries = require("./typedefs-resolvers/_queries");
+const mutations = require("./typedefs-resolvers/_mutations");
+const enums = require("./typedefs-resolvers/_enums");
+const teams = require("./typedefs-resolvers/teams");
+const people = require("./typedefs-resolvers/people");
+const roles = require("./typedefs-resolvers/roles");
+const equipments = require("./typedefs-resolvers/equipments");
+const softwares = require("./typedefs-resolvers/softwares");
+const supplies = require("./typedefs-resolvers/supplies");
+const tools = require("./typedefs-resolvers/tools");
+const givens = require("./typedefs-resolvers/givens");
 
 const typeDefs = [
-    queries,
-    mutations,
-    enums,
-    teams.typeDefs,
-    people.typeDefs,
-    roles.typeDefs,
-    equipments.typeDefs,
-    softwares.typeDefs,
-    supplies.typeDefs,
-    tools.typeDefs,
-    givens.typeDefs
-]
+  queries,
+  mutations,
+  enums,
+  teams.typeDefs,
+  people.typeDefs,
+  roles.typeDefs,
+  equipments.typeDefs,
+  softwares.typeDefs,
+  supplies.typeDefs,
+  tools.typeDefs,
+  givens.typeDefs,
+];
 
 const resolvers = [
-    teams.resolvers,
-    people.resolvers,
-    roles.resolvers,
-    equipments.resolvers,
-    softwares.resolvers,
-    supplies.resolvers,
-    tools.resolvers,
-    givens.resolvers
-]
+  teams.resolvers,
+  people.resolvers,
+  roles.resolvers,
+  equipments.resolvers,
+  softwares.resolvers,
+  supplies.resolvers,
+  tools.resolvers,
+  givens.resolvers,
+];
 
-const server =  new ApolloServer({typeDefs, resolvers})
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({url}) => {
-    console.log(`🚀  Server ready at ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+/*
+GraphQL의 강점 나의 언어로 요약
+: 필요한 정보들만 원하는 만큼 부족하지도 과하지도 않게 한번에 가져오고 요청할 수 있음
+*/
